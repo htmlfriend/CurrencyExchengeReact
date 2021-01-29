3de52acbfd5942565573aff624fc12740f053808
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-29 16:29:53 -0500

    Updates

---
+++ precache-manifest.be03f9ce3a007efdca788216d8919e2a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "81fe2cc7dc78f1dd85a1",
+    "url": "/CurrencyEchengeReact/static/css/main.c2cfb957.chunk.css"
+  },
+  {
+    "revision": "81fe2cc7dc78f1dd85a1",
+    "url": "/CurrencyEchengeReact/static/js/main.81fe2cc7.chunk.js"
+  },
+  {
+    "revision": "ee24a1298dd2f0461a59",
+    "url": "/CurrencyEchengeReact/static/js/1.ee24a129.chunk.js"
+  },
+  {
+    "revision": "13099cc40a79190543b1",
+    "url": "/CurrencyEchengeReact/static/js/runtime~main.13099cc4.js"
+  },
+  {
+    "revision": "dc8dc7aaaa4da39106478ec24e939fbf",
+    "url": "/CurrencyEchengeReact/static/media/CNY.dc8dc7aa.png"
+  },
+  {
+    "revision": "e1c9768d02aff22f203c4f93d0fe670d",
+    "url": "/CurrencyEchengeReact/static/media/EUR.e1c9768d.png"
+  },
+  {
+    "revision": "38c1328568118ccee45db1e5b93b5e71",
+    "url": "/CurrencyEchengeReact/static/media/GBP.38c13285.png"
+  },
+  {
+    "revision": "9987aabc6c809425e08b51935120d51a",
+    "url": "/CurrencyEchengeReact/static/media/JPY.9987aabc.png"
+  },
+  {
+    "revision": "0c79b82e54d1e0e2c1ef7f03be71a687",
+    "url": "/CurrencyEchengeReact/static/media/RUB.0c79b82e.png"
+  },
+  {
+    "revision": "ff97e132da519724034302a4a92750ef",
+    "url": "/CurrencyEchengeReact/static/media/USD.ff97e132.png"
+  },
+  {
+    "revision": "def013f01bbc000c5dbd3386d8bedefc",
+    "url": "/CurrencyEchengeReact/index.html"
+  }
+];
