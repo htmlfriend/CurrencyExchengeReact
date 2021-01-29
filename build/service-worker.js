2a71ba028abc0f0684e0443d13bbce8a1f4ef697
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-29 16:33:41 -0500

    deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/moviewatch/precache-manifest.29978a294d9d5843d082fd1fff06421e.js"
+  "/CurrencyEchengeReact/precache-manifest.be03f9ce3a007efdca788216d8919e2a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/moviewatch/index.html", {
+workbox.routing.registerNavigationRoute("/CurrencyEchengeReact/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
